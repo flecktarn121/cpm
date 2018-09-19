9b27c3dfc9e5f801d06a1ec46b254c8aba6fd482
Angel <UO258654@uniovi.es>
2018-09-19 18:33:45 +0200

    Initial commit for practice 1

---
+++ Practice1/src/gui/MainWindow.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+	public MainWindow() {
+
+	}
+
+	public static void main(String[] args) {
+		MainWindow mw = new MainWindow();
+		mw.setVisible(true);
+	}
+
+}
